68f483b66c9600da25903833ee296cf0041ca82a
Jarrett Meyer <meyer_jarrett@lilly.com>
2019-02-26 08:59:28 -0500

    Updated app.sh

--- app.sh
+++ app.sh
@@ -3,4 +3,8 @@
 #
 # This script will be run by OpenShift's launcher.
 #
-jupyter notebook --no-browser --ip=0.0.0.0
+# OpenShift's default port is 8080. Either you need to change the port that
+# Jupyter runs on, or you need to change the port in the OpenShift settings.
+# This is the easier of the two solutions.
+#
+jupyter notebook --no-browser --ip=0.0.0.0 --port=8080
